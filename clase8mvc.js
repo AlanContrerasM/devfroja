c56c4f485e3c650220a7d8fb81d782a63b91521e
bullstreeter <alanrcontrerasm@gmail.com>
2018-06-13 10:57:16 -0700

    updateando archivos

---
+++ clase8mvc.js
@@ -0,0 +1,8 @@
+//mvc es modelo, vistas, y controladores.
+//patron de diseño de software, en pocas palabras que este en capas.
+
+//express es un micro framework
+//con javascript podemos hacer todo, pero los frameworks son bibliotecas con chochos
+//hay frameworks para back end y front-end
+//express es un micro framework por que ayuda a construir un back-end muy rapido, 
+//pero te deja muchas cosas al aire.
